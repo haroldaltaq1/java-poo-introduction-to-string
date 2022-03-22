7319a160e30fe347977c1db7f020ed5b6149d8eb
Harold Altamiranda <harold.altamiranda@i2btech.com>
2022-03-22 15:40:53 -0500

    compare string

--- src/ExampleString.java
+++ src/ExampleString.java
@@ -1,6 +1,9 @@
 public class ExampleString {
 
     public static void main(String[] args) {
-
+        String curso = "Programación Java";        
+        String curso2 = new String("Programación Java");
+        boolean esIgual = curso == curso2;
+        System.out.println("esIgual = " + esIgual);
     }
 }
